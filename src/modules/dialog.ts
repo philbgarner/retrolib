d43c94244e9f5c6f5b1318e4eaad7ee9aa36cf16
Phil Garner <phil.b.garner@gmail.com>
2023-12-31 14:13:54 -0500

    Stubbing out dialog class: should follow JRPG conventions.

---
+++ src/modules/dialog.ts
@@ -0,0 +1,10 @@
+import Scene from './scene'
+
+/**
+ * Dialog scene flavour: slowly display specified text and when the Rect
+ * width is filled, wrap to the next line and continue. Also pause when
+ * the Rect is fully filled waiting for input to continue.
+ */
+class Dialog extends Scene {
+
+}
